ea357cb526f4e5983061b75f024639c41d1ef8b3
takayanagi shotaro <s.takayanagi1116@gmail.com>
2020-10-29 15:31:59 +0900

    pages/index.vueをtodo.vueを移した

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,100 @@
 <template>
-  <v-row justify="center" align="center" class="mx-auto">
-    <v-col cols="12" sm="8" md="6">
-      <div class="mt-4 mb-8 text-center">
-        <div class="text-h4 title-text font-weight-bold">Todo App</div>
-      </div>
-      <v-col cols="12" class="px-4">
-        <Form @submit="addCard" />
-        <CardList :card-list="cardList" @submit="deleteCard" />
+  <v-container justify="center" align="center" class="mx-auto">
+    <v-row cols="6" lg="3">
+      <v-col class="mt-4 mb-8 text-center">
+        <div class="text-h4 title-text font-weight-bold">Minimal App</div>
       </v-col>
-    </v-col>
-  </v-row>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="6" md="4">
+        <v-btn
+          elevation="2"
+          height="18vh"
+          width="100%"
+          class="pa-2 mx-auto col-btn"
+          tile
+          outlined
+          to="/todo"
+        >
+          todo
+        </v-btn>
+      </v-col>
+      <v-col cols="6" md="4">
+        <v-btn
+          elevation="2"
+          height="18vh"
+          width="100%"
+          class="pa-2 mx-auto"
+          tile
+          outlined
+        >
+          a
+        </v-btn>
+      </v-col>
+      <v-col cols="6" md="4">
+        <v-btn
+          elevation="2"
+          height="18vh"
+          width="100%"
+          class="pa-2 mx-auto"
+          tile
+          outlined
+        >
+          a
+        </v-btn>
+      </v-col>
+      <v-col cols="6" md="4">
+        <v-btn
+          elevation="2"
+          height="18vh"
+          width="100%"
+          class="pa-2 mx-auto"
+          tile
+          outlined
+        >
+          a
+        </v-btn>
+      </v-col>
+      <v-col cols="6" md="4">
+        <v-btn
+          elevation="2"
+          height="18vh"
+          width="100%"
+          class="pa-2 mx-auto"
+          tile
+          outlined
+        >
+          a
+        </v-btn>
+      </v-col>
+      <v-col cols="6" md="4">
+        <v-btn
+          elevation="2"
+          height="18vh"
+          width="100%"
+          class="pa-2 mx-auto"
+          tile
+          outlined
+        >
+          a
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
-@Component
-export default class index extends Vue {
-  cardList: Object[] = []
-
-  addCard(card: Object) {
-    this.cardList.push(card)
-  }
-
-  deleteCard(index: number) {
-    this.cardList.splice(index, 1)
-  }
-}
+@Component({})
+export default class index extends Vue {}
 </script>
 
 <style lang="postcss" scoped>
 .title-text {
   color: #626262;
+}
+
+.col-btn {
+  text-decoration: none;
 }
 </style>
---
+++ pages/todo.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-row justify="center" align="center" class="mx-auto">
+    <v-col cols="12" sm="8" md="6">
+      <div class="mt-4 mb-8 text-center">
+        <div class="text-h4 title-text font-weight-bold">Todo App</div>
+      </div>
+      <v-col cols="12" class="px-4">
+        <Form @submit="addCard" />
+        <CardList :card-list="cardList" @submit="deleteCard" />
+      </v-col>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+@Component
+export default class todo extends Vue {
+  cardList: Object[] = []
+
+  addCard(card: Object) {
+    this.cardList.push(card)
+  }
+
+  deleteCard(index: number) {
+    this.cardList.splice(index, 1)
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.title-text {
+  color: #626262;
+}
+</style>
